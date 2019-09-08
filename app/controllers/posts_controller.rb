d0d83413162dd838a84adf5db4600e0b7b44f8d7
EC2 Default User <ec2-user@ip-172-31-43-70.ap-northeast-1.compute.internal>
2019-09-08 01:04:15 +0000

    modelにcreated_at descを設定、controllerから削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:show, :new, :create, :edit, :update, :destroy]
   def index
-    @posts = Post.order("created_at DESC").page(params[:page]).per(5)
+    @posts = Post.all.page(params[:page]).per(5)
   end 
   
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
 end
