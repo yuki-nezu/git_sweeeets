bb50ee055061b04904ecbd67f440c6c71c6691bf
EC2 Default User <ec2-user@ip-172-31-43-70.ap-northeast-1.compute.internal>
2019-09-08 07:04:59 +0000

    already_likedの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post  
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
 end
