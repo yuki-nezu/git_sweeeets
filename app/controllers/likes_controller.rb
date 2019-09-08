0a054c4efb1b6d309d030f816da7a8c7abb5a22c
EC2 Default User <ec2-user@ip-172-31-43-70.ap-northeast-1.compute.internal>
2019-09-08 06:58:38 +0000

    likes_controllerの定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  
+  def create
+    @like = current_user.likes.create(post_id: params[:post_id])
+    redirect_to :root
+  end 
+  
+  def destoy
+    @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @like.destroy
+    redirect_to :root
+  end 
+  
 end
