1e06816d2a2877fcfd6f93cd3ebb3869f41d97ad
EC2 Default User <ec2-user@ip-172-31-43-70.ap-northeast-1.compute.internal>
2019-09-23 03:28:45 +0000

    いいねがついた投稿を削除できるようにした

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   default_scope -> { order(created_at: :desc) }
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   
   def like_user(user_id)
