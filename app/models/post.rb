3f24d9f91b4d69c8379e029857ac939c3b163656
EC2 Default User <ec2-user@ip-172-31-43-70.ap-northeast-1.compute.internal>
2019-09-30 07:29:06 +0000

    minimagickでリサイズを可能に

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  mount_uploader :image, ImageUploader
+   mount_uploader :image, ImageUploader
   default_scope -> { order(created_at: :desc) }
   belongs_to :user
   has_many :likes, dependent: :destroy
