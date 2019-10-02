ff7b60b1b806f925a165e27d2c5a377e76c50980
EC2 Default User <ec2-user@ip-172-31-43-70.ap-northeast-1.compute.internal>
2019-10-02 04:24:43 +0000

    Procfile updated

--- config/puma.rb
+++ config/puma.rb
@@ -16,9 +16,10 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
-
 
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
