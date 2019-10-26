fd5a5968b20309cdda0bf65a437a719b11dd5bfb
EC2 Default User <ec2-user@ip-172-31-43-70.ap-northeast-1.compute.internal>
2019-10-26 07:04:53 +0000

    gitignore設定

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = posts(:one)
+  end
+
+  test "should get index" do
+    get posts_url
+    assert true
+  end
+
+  test "should create post" do
+    Post.create(text: "text", user_id: 1)
+    assert true
+  end
+
+  test "should show post" do
+    get post_url(@post)
+    assert true
+  end
+  
+  test "should edit post" do
+    get edit_post_url(@post)
+    assert true
+  end
+  
+  test "should update post" do
+    patch post_url(@post)
+    @post.update(text: "text2", user_id: 1)
+    assert true
+  end
+  
+  test "should destroy post" do
+    delete post_url(@post)
+    assert true
+  end
+  
 end
